db290342f25d4f6ca49467d2df8855b1bc3b6c7e
Scott Hudson <shudson5523@gmail.com>
2019-08-11 15:36:12 -0400

    updated index.html to Landing.js in server.js

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ if (process.env.node_env === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build','Landing.js'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'))
   });
 }
 
